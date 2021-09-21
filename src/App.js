891120f9e2d0729c47047be0f2e78edfa84882c4
MatheusGorga <matheusgobar@gmail.com>
2021-09-21 01:22:57 -0300

    api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import List from './components/List';
+import { useEffect, useState } from "react";
+import "./App.css";
+import List from "./components/List";
+import api from "./services/api";
+
 
 function App() {
- 
-  /*function maisContador(){
+  const [userName, setUserName] = useState("MatheusGorga");
+  const [userData, setUserData] = useState([]);
+
+useEffect(() =>{
+  const localStorageuserData = localStorage.getItem('@reactapp:GITHUB_USER_DATA')
+  setUserData(JSON.parse(localStorageuserData))
+}, [])
+
+ async function getUserGitHubData(){
+   const {data} = await api.get(userName)
+   localStorage.setItem('@reactapp:GITHUB_USER_DATA', JSON.stringify(data))
+   setUserData(data)
+  }
+
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button onClick={getUserGitHubData} > 🔍Pesquisar</button>
+
+      <div>
+      <h3>{userData.name}</h3>
+      <img src={userData.avatar_url}/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
+
+/*function maisContador(){
     setTimeout(() => {
       setContador( (preCounter) => preCounter + 1)
     }, 2000)
@@ -14,15 +50,3 @@ function App() {
       setContador( (preCounter) => preCounter - 1)
     }, 2000)
   }*/
-
-  const [showList, setShowList] = useState(true)
-
-  return (
-    <div className="App">
-      <h1>Ola</h1>
-      {showList && <List/>} 
-    </div>
-  );
-}
-
-export default App;
